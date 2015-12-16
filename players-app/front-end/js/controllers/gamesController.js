177f27affd1046888b502c8fecca892fbaf17433
NJonsey <nyejones@hotmail.co.uk>
2015-12-16 19:28:07 +0000

    console logs removed

--- players-app/front-end/js/controllers/gamesController.js
+++ players-app/front-end/js/controllers/gamesController.js
@@ -17,7 +17,6 @@ function GamesController(Game, uiGmapGoogleMapApi, $scope) {
  self.addGame = function() {
    Game.save(self.newGame, function(newGame) {
      self.all.push(newGame);
-     console.log(newGame);
      self.newGame = {};
    })
  };
@@ -50,17 +49,11 @@ function GamesController(Game, uiGmapGoogleMapApi, $scope) {
      info.end_time + '</li>' + '<li>' + info.number_of_players + '</li>' + '<li>' + 
      info.comment + '</li>' + '<li>' + info.placeName + '</ul></div>';
 
-     console.log(marker.content);
-
      maps.event.addListener(marker, 'click', function () {
-      console.log('hello')
-      console.log(search_map)
          infoWindow.setContent('<h2 class="infoWindowTitle">' + marker.title + '</h2>' + marker.content);
-         console.log(infoWindow.content)
          infoWindow.open(search_map, marker);
      });
 
-     console.log(marker);
      self.markers.push(marker);
    }
 
@@ -71,7 +64,6 @@ function GamesController(Game, uiGmapGoogleMapApi, $scope) {
    $scope.openInfoWindow = function (e, selectedMarker) {
           e.preventDefault();
           maps.event.trigger(selectedMarker, 'click');
-          console.log("click");
       }
 
   $scope.$watch('nas', function (newValue, oldValue) {
@@ -128,120 +120,5 @@ function GamesController(Game, uiGmapGoogleMapApi, $scope) {
 
 };
 
-
-
-// angular
-// .module('GetAGame')
-// .controller('GamesController', GamesController);
-
-// GamesController.$inject = ['Game', 'uiGmapGoogleMapApi', '$scope'];
-// function GamesController(Game, uiGmapGoogleMapApi, $scope) {
-
-//   var self = this;
-
-//   this.all = Game.query();
-//   this.newGame = {};
-//   this.place = {};
-//   this.sportSearch = "";
-//   this.numberSearch = "";
-//   this.markers = [];
-
-//   self.addGame = function() {
-//     Game.save(self.newGame, function(newGame) {
-//       self.all.push(newGame);
-//       console.log(newGame);
-//       self.newGame = {};
-//     })
-//   };
-
-//   uiGmapGoogleMapApi.then(function(maps) {
-
-//     var search_map = new maps.Map(
-//       document.getElementById('search_map'),
-//       {
-//         center: {
-//           lat: 51.5081,
-//           lng: -0.1000
-//       },
-//       zoom: 14
-//     });
-
-//     var createMarker = function(info) {
-
-//       var marker = new maps.Marker({
-//                map: search_map,
-//                position: new maps.LatLng(info.lat, info.lng),
-//                title: info.sport
-//            });
-//       console.log(marker);
-
-//       self.markers.push(marker);
-//       console.log(self.markers)
-//     }
-
-//     console.log(self.markers)
-
-//     for (i = 0; i < self.all.length; i++) {
-//       createMarker(self.all[i]);
-//     }
-
-//         console.log(self.all.length)
-
-//   });
-
-//   uiGmapGoogleMapApi.then(function(maps) {
-
-//     var map = new maps.Map(
-//       document.getElementById('main-map'),
-//       {
-//         center: {
-//           lat: 51.5081,
-//           lng: -0.1000
-//       },
-//       zoom: 14
-//     });
-
-//     var input = document.getElementById('google-place');
-
-//     var autocomplete = new maps.places.Autocomplete(input);
-
-//     var marker = new maps.Marker({
-//       map: map
-//     });
-
-//     maps.event.addListener(autocomplete, 'place_changed', function() {
-
-//       self.place = autocomplete.getPlace();
-      
-//       self.newGame.placeId = self.place.id;
-//       self.newGame.placeName = self.place.name;
-
-//       self.newGame.lat = self.place.geometry.location.lat();
-//       self.newGame.lng = self.place.geometry.location.lng();
-
-//       marker.setPlace({
-//         placeId: self.place.place_id,
-//         location: self.place.geometry.location
-//       });
-
-//       marker.setVisible(true);
-//       map.panTo(marker.getPlace().location);
-
-//     });
-//   })
-
-//   $scope.$watch('nas',
-
-//   function (newValue, oldValue) {
-//       for (jdx in $scope.markers) {
-//           $scope.markers[jdx].setMap(null);
-//       }
-//       $scope.markers = [];
-//       for (idx in $scope.nas) {
-//           createMarker($scope.nas[idx]);
-//       }
-//   }, true);
-
-// };
 
   
