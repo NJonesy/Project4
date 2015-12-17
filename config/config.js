c4534861051dc299db84bfd306ed2bc81598c010
NJonsey <nyejones@hotmail.co.uk>
2015-12-17 11:50:56 +0000

    added port in config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': 'jsonwebtokensaregreat',
   'database': process.env.MONGOLAB_URI || 'mongodb://localhost:27017/players-app',
-  'port': process.env.PORT || 3000;
+  'port': process.env.PORT || 3000
 };
